59f9e5f213a48e810f3a1f7f64e33bbcc0d24206
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-03-14 20:31:58 +0800

    ✨ feat: remove chat function

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,5 +1,5 @@
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline"
-import { collection, orderBy, query } from "firebase/firestore"
+import { collection, deleteDoc, doc, orderBy, query } from "firebase/firestore"
 import { useSession } from "next-auth/react"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
@@ -8,7 +8,7 @@ import { useCollection } from "react-firebase-hooks/firestore"
 import { db } from "../firebase"
 
 type Props = {
-    id: string
+  id: string
 }
 
 function ChatRow({ id }: Props) {
@@ -20,8 +20,8 @@ function ChatRow({ id }: Props) {
 
   const [messages] = useCollection(
     query(
-        collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'),
-        orderBy('createdAt', 'asc')
+      collection(db, 'users', session?.user?.email!, 'chats', id, 'messages'),
+      orderBy('createdAt', 'asc')
     )
   )
 
@@ -31,13 +31,18 @@ function ChatRow({ id }: Props) {
     setActive(pathname.includes(id))
   }, [pathname])
 
+  const removeChat = async() => {
+    await deleteDoc(doc(db, 'users', session?.user?.email!, 'chats', id));
+    router.replace('/');
+  }
+
   return (
     <Link href={`/chat/${id}`} className={`chatRow justify-center ${active && 'bg-gray-700/50'}`}>
         <ChatBubbleLeftIcon className="h-5 w-5"/>
         <p className="flex-1 hidden md:inline-flex truncate">
             { messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
         </p>
-        <TrashIcon className="h-5 w-5 text-[#c5c5d2] hover:text-white"/>
+        <TrashIcon onClick={removeChat} className="h-5 w-5 text-[#c5c5d2] hover:text-white"/>
     </Link>
   )
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { collection } from "firebase/firestore"
+import { collection, orderBy, query } from "firebase/firestore"
 import { signOut, useSession } from "next-auth/react"
 import { useCollection } from "react-firebase-hooks/firestore"
 import { db } from "../firebase"
@@ -12,7 +12,7 @@ function SideBar() {
   const { data: session} = useSession()
 
   const [chats, loading, error] = useCollection(
-    session && collection(db, 'users', session.user?.email!, 'chats')
+    session && collection(db, 'users', session.user?.email!, 'chats'),
   )
 
 //   console.log(chats);
