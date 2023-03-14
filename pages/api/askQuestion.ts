b93f99fe528449bd334a0d53a5f4530077643030
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-03-15 00:19:34 +0800

    🔨 to: openai respone status, unfix 429

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import query from '../../lib/queryAPI';
+import admin from 'firebase-admin';
+import { adminDB } from '../../firebaseAdmin';
 
 type Data = {
   answer: string
@@ -23,7 +26,19 @@ export default async function handler(
   }
 
   //  ChatGPT query 
-  //const respone = await query(prompt, chatID, model)
+  const respone = await query(prompt, chatID, model)
 
-  
+  const message: Message = {
+    text: respone || "ChatGPT was unable to find an answer",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+        _id: 'ChatGPT',
+        name: 'ChatGPT',
+        avatar: 'https://links.papareact.com/89k',
+    },
+  }
+
+  await adminDB.collection('users').doc(session?.user?.email).collection('chats').doc(chatID).collection('messages').add(message);
+
+  res.status(200).json({ answer: message.text })
 }
