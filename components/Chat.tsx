01072ba3557e7d67cf7bc0968156b295cfb91b57
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-03-15 00:50:01 +0800

    💄 feat: GPT's chat style

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,12 +1,28 @@
+'use client'
+
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../lib/firebase";
+import Message from "./Message";
 
 type Props = {
     chatID: string
 }
 
 function Chat({ chatID }: Props) {
+  const { data: session } = useSession();
+
+  const [messages] = useCollection(session && query(
+    collection(db, 'users', session?.user?.email!, 'chats', chatID, 'messages'),
+    orderBy('createdAt', 'asc')
+  ))
+
   return (
     <div className="flex-1">
-      Chat
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()}/>
+      ))}
     </div>
   )
 }
